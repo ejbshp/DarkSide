b204a49056570af5cb7adf9ef6cbded04503914d
Ellie <eleanor.bishop-3@postgrad.manchester.ac.uk>
2022-03-07 12:52:12 +0000

    using s2 binning info

--- Neutrino spectrum/my_cevns.py
+++ Neutrino spectrum/my_cevns.py
@@ -1,48 +1,59 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 22 2021
+my_cevns.py
 
-March 2 2022 - Editing to add in sampling from the energy spec as in cenns.py
+March 2 2022 - Copy of neutrinospec_conversion_ERtoPE - cutting out a binning method.
+March 4 2022 - Editing to change the bins, bins in the response function are half electron.
 
 author: EB
 
 Converts neutrino spectrum data from recoil energy into Photoelectrons and then writes to file
 
 """
+
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import random
 import bisect
 
-# loading in SM neutrino data from theorists - New SM
-er , spec = np.loadtxt('argon_spec.txt', delimiter=' ')
+# loading in SM neutrino data from Andrew
+er , spec = np.loadtxt('data/argon_spec.txt', delimiter=' ')
 # convert er from GeV to KeV
 er = er * 1e6
+
+####### remove this - using paolo's spec
 # loading in old SM - already in 
-old_er , old_spec = np.loadtxt('rateTOTAr_old_spec_for_comparison.txt', delimiter='\t', unpack=True)
+er , spec = np.loadtxt('data/rateTOTAr_old_spec_for_comparison.txt', delimiter='\t', unpack=True)
 
 
-
-
-# importing energy bins
+# importing energy bins - from response func
 col_list = ['energy_bin_start_kev','energy_bin_end_kev']
-df = pd.read_csv('remake_ds20k_ne_response_nr.csv', usecols=col_list)
+df = pd.read_csv('data/remake_ds20k_ne_response_nr.csv', usecols=col_list)
 ebin_start = df.energy_bin_start_kev.to_list()
 ebin_end = df.energy_bin_end_kev.to_list()
 
-# importing electron bin probabilities
-df2 = pd.read_csv('remake_ds20k_ne_response_nr.csv', usecols=range(3,203))
-ne_probs = df2.values.tolist()
+# importing electron bin probabilities - from response func
+df2 = pd.read_csv('data/remake_ds20k_ne_response_nr.csv', usecols=range(3,203))
+s2_probs_list = df2.values.tolist()
 
-   
-#%%
+# importing binning information - from binning info file - describes how bins relate to NPE
+col_list = ['linear_center_ne']
+df = pd.read_csv('data/ds20k_s2_binning_info.csv', usecols=col_list)
+s2_ne_bin_centres = df.linear_center_ne.to_list()
+
+
+
 # =============================================================================
 # Converting er to No of PE - multiplying by width
 # =============================================================================
 
-# empty array for np count
-pe_count = np.zeros(len(ne_probs[0]))
+# Empty array for counting the number of events in each s2 bin
+s2_bin_count = np.zeros(len(s2_probs_list[0]))
+
+# multiply to sample more - smooth things out
+mult = 1000
 
 # getting PE count for the data given
 for j in range(len(er)-1): # can't calculate diff for last point
@@ -50,154 +61,79 @@ for j in range(len(er)-1): # can't calculate diff for last point
     rate = spec[j]
     # multiply by the width of the energy bin - approx as distance between each point
     width = er[j+1]-er[j]
-    rate = width * rate
+    rate = width * rate * mult # mult to sample more
     # if energy is less than 0.1 no PE produced - so can skip sampling
     if energy < 0.1:
-        pe_count[0] += rate
+        s2_bin_count[0] += rate
     else:
-        # loop though to find energy bin
-        for i in range(len(ebin_end)):
-            emin = ebin_start[i]
-            emax = ebin_end[i]
-            if energy >= emin and energy < emax:
-                # number of times we sample = rate for that energy bin
-                while rate > 0:  
-                    # sampling from our distribution for that energy bin
-                    ne = random.choices(list(range(0,200)), ne_probs[i])
-                    # adding to count
-                    pe_count[ne.pop()] += 1
-                    rate -= 1
-                break
+        # find index of energy bin - note doesn't count from zero
+        index = bisect.bisect_left(ebin_start, energy) - 1
+        # get response func probabilities for that energy bin
+        # if the s2 probs empty for that E it will be bin 0 - skip sampling
+        if sum(s2_probs_list[index]) == 0.0:
+            s2_bin_count[0] += rate
+        else:  # need to sample
+            # if not empty normalise so all the values add up to 1
+            normaliser = 1.0 / sum(s2_probs_list[index])
+            s2_bin_probs = [i * normaliser for i in s2_probs_list[index]] # just multipying the whole list by norm
             
-
-
-# Ne bins
-bins = np.arange(len(pe_count))
-
-# =============================================================================
-# Writing data to file
-# =============================================================================
-
-file = open('argon_spec_PE_multw.txt', 'w')
-for n in bins:
-    file.write(str(n) + ' ' + str(pe_count[n]) + '\n')
-file.close()
-
-diff = []
-for i in range(len(er)-1):
-    diff.append(er[i+1]-er[i])
-    
-    
-
-
-
-#%% Rate binned in the same way as the response function
-#!!! this is weird - doesn't really work
-
-col_list = ['energy_bin_end_kev']
-df = pd.read_csv('remake_ds20k_ne_response_nr.csv', usecols=col_list)
-max_e = df.energy_bin_end_kev.to_list()
-
-
-binned_rate = []
-max_e_check = []
-n_check = 0
-last_ind = 0
-
-pe_count = np.zeros(len(ne_probs[0]))
-
-for e in max_e:
-    # index of last value below 0.1
-    ind = bisect.bisect_left(er, e) -1
-    x = er[last_ind:ind]
-    y = spec[last_ind:ind]
-    rate = np.trapz(y,x)
-    # add to list
-    max_e_check.append(x)
-    binned_rate.append(rate)
-    n_check += len(x)
-    last_ind = ind
-    
-for i in range(len(binned_rate)):
-    rate = binned_rate[i]
-    while rate > 0:  
-        # sampling from our distribution for that energy bin
-        ne = random.choices(list(range(0,200)), ne_probs[i])
-        # adding to count
-        pe_count[ne.pop()] += 1
-        rate -= 1
-    
-
-
-# Ne bins
-bins = np.arange(len(pe_count))
-
-# =============================================================================
-# Writing data to file
-# =============================================================================
-
-file = open('argon_spec_PE_rebin.txt', 'w')
-for n in bins:
-    file.write(str(n) + ' ' + str(pe_count[n]) + '\n')
-file.close()
-
-diff = []
-for i in range(len(er)-1):
-    diff.append(er[i+1]-er[i])
-    
-    
-    
-#%%
-# =============================================================================
-# Converting er to No of PE - multiplying by width - old spec
-# =============================================================================
-
-# empty array for np count
-pe_count = np.zeros(len(ne_probs[0]))
-
-# getting PE count for the data given
-for j in range(len(old_er)-1): # can't calculate diff for last point
-    energy = old_er[j]
-    rate = old_spec[j]
-    # multiply by the width of the energy bin - approx as distance between each point
-    width = old_er[j+1]-old_er[j]
-    rate = width * rate
-    # if energy is less than 0.1 no PE produced - so can skip sampling
-    if energy < 0.1:
-        pe_count[0] += rate
-    else:
-        # loop though to find energy bin
-        for i in range(len(ebin_end)):
-            emin = ebin_start[i]
-            emax = ebin_end[i]
-            if energy >= emin and energy < emax:
-                # number of times we sample = rate for that energy bin
-                while rate > 0:  
-                    # sampling from our distribution for that energy bin
-                    ne = random.choices(list(range(0,200)), ne_probs[i])
-                    # adding to count
-                    pe_count[ne.pop()] += 1
-                    rate -= 1
-                break
+            # sample from the probs_list
+            # number of times we sample = rate for that energy bin
+            while rate > 0:  
+                # sampling from our distribution for that energy bin
+                s2_bin_no    = np.random.choice( len(s2_bin_probs), p=s2_bin_probs, size=None)
+                # adding to count
+                s2_bin_count[s2_bin_no] += 1
+                rate -= 1
             
+            
+# now need to convert s2 bin count to PE
+#!!! note may need to scale inside the bin as in cenns.py line 129
+# atm will just plot the s2 bin centres against the count
+# divding by the multiplying factor
+s2_bin_count /= mult
 
 
 # Ne bins
-bins = np.arange(len(pe_count))
+bins = np.arange(len(s2_bin_count))
 
 # =============================================================================
 # Writing data to file
 # =============================================================================
 
-file = open('old_argon_spec_PE_multw.txt', 'w')
+file = open('data/argon_spec_PE_multw.txt', 'w')
 for n in bins:
-    file.write(str(n) + ' ' + str(pe_count[n]) + '\n')
+    file.write(str(n) + ' ' + str(s2_bin_count[n]) + '\n')
 file.close()
 
 diff = []
 for i in range(len(er)-1):
     diff.append(er[i+1]-er[i])
     
+
+# =============================================================================
+# Plotting
+# =============================================================================
     
-    
+firstbin = 0
+lastbin = 50
+
+# loading data
+ds20k_cevns = np.loadtxt('data/ds20k-cenns_bkgrd.dat',delimiter=' ')[firstbin:lastbin,1]
+bins = np.loadtxt('data/ds20k-cenns_bkgrd.dat',delimiter=' ')[firstbin:lastbin,0]
+new_cevns = np.loadtxt('data/argon_spec_PE_multw.txt',delimiter=' ')[firstbin:lastbin,1]
+new_bins = np.loadtxt('data/argon_spec_PE_multw.txt',delimiter=' ')[firstbin:lastbin,0]
+
+# convert old cevns into events/tyr - dividing by ds20k exposure in tyr
+ds20k_cevns = ds20k_cevns / 100
+
+# plotting
+f=plt.figure(figsize=(10,8))
+plt.plot(bins,ds20k_cevns, label='Old CEvNS already in PE')
+plt.plot(s2_ne_bin_centres, s2_bin_count, label='Using s2 binning info')
+
+plt.xlabel('Number of electrons',fontsize=26)
+plt.ylabel('Events per tyr',fontsize=26) 
+plt.yscale('log')
+plt.legend(fontsize=18,frameon=False,loc=9)
 
