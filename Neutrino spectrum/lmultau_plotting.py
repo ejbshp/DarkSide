5cd1cf6629e34934af29b58ec89ce78f54353b4e
Ellie <eleanor.bishop-3@postgrad.manchester.ac.uk>
2022-05-06 16:56:18 +0100

    better looking plots!

--- Neutrino spectrum/lmultau_plotting.py
+++ Neutrino spectrum/lmultau_plotting.py
@@ -20,6 +20,7 @@ import glob
 import matplotlib.pyplot as plt
 import matplotlib
 import bisect
+from matplotlib.cm import ScalarMappable
 
 from func_my_cevns import spectope
 
@@ -37,6 +38,19 @@ def getindex(g, m):
 
     index = df_MuTau[(df_MuTau['g_x'] < mult*g) & (df_MuTau['g_x'] > mult2*g) & (df_MuTau['m_A']< 1.05*m) & (df_MuTau['m_A'] > 0.95*m)].index[0]
     return index
+
+def getratio(index):
+    '''
+    returns ratio of bsm/sm for a given row ion df
+    
+    index: index of row in df
+
+    '''
+    # points at which we want to sample
+    x_sample = sm_er # sm spec starts at a higher ER
+    ratio = np.interp(x_sample, df_MuTau.iloc[index].ERs, df_MuTau.iloc[index].spec) / sm_spec
+    
+    return ratio
 
 # In[4]: LOADING IN DATA
 
@@ -93,7 +107,7 @@ plt.loglog(sm_er*1e6, sm_spec) # sm only from 1e-2 whereas other spec are from 1
 fig=plt.figure(3,figsize=(10,8))
 
 # plotting the diff with gx and M
-plt.scatter(df_MuTau['m_A'].values, df_MuTau['g_x'].values, c=df_MuTau['diff'].values, norm=matplotlib.colors.LogNorm(), vmax = 2)
+plt.scatter(df_MuTau['m_A'].values, df_MuTau['g_x'].values, c=df_MuTau['diff'].values, norm=matplotlib.colors.LogNorm(), vmax = 2, cmap = plt.get_cmap('Spectral'))
 plt.ylabel('$g_x$ [GeV]')
 plt.xlabel('$m_A$ [GeV]')
 plt.xscale('log')
@@ -132,37 +146,48 @@ g = getindex(1e-2, fixed_mass)
 
 
 # plotting
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(25,10))
+fig.suptitle('Fixed mass: m_A =' + str(fixed_mass) + ' [GeV]', fontsize = 20)
 
-f=plt.figure(5,figsize=(10,8))
-plt.xlabel(r'$E_R\,\,\,\left[\rm{keV}\right]$', size=16)
-plt.ylabel(r'$\rm{d}R/\rm{d}E_R\,\,\,\left[\rm{keV}^{-1}\,\,\rm{ton}^{-1}\,\,\rm{yr}^{-1}\right]$', size=16)
-
-plt.title(' m_A = ' + str(fixed_mass))
-
-plt.loglog(df_MuTau.iloc[a].ERs*1e6, df_MuTau.iloc[a].spec, label = 'g_x = ' + str(df_MuTau.iloc[a].g_x) )
-plt.loglog(df_MuTau.iloc[b].ERs*1e6, df_MuTau.iloc[b].spec, label = 'g_x = ' + str(df_MuTau.iloc[b].g_x) )
-plt.loglog(df_MuTau.iloc[c].ERs*1e6, df_MuTau.iloc[c].spec, label = 'g_x = ' + str(df_MuTau.iloc[c].g_x) )
-plt.loglog(df_MuTau.iloc[d].ERs*1e6, df_MuTau.iloc[d].spec, label = 'g_x = ' + str(df_MuTau.iloc[d].g_x) )
-plt.loglog(df_MuTau.iloc[e].ERs*1e6, df_MuTau.iloc[e].spec, label = 'g_x = ' + str(df_MuTau.iloc[e].g_x) )
-plt.loglog(df_MuTau.iloc[g].ERs*1e6, df_MuTau.iloc[g].spec, label = 'g_x = ' + str(df_MuTau.iloc[g].g_x) )
+# Er spec for the 6 points
+ax1.set_xlabel(r'$E_R\,\,\,\left[\rm{keV}\right]$', size=16)
+ax1.set_ylabel(r'$\rm{d}R/\rm{d}E_R\,\,\,\left[\rm{keV}^{-1}\,\,\rm{ton}^{-1}\,\,\rm{yr}^{-1}\right]$', size=16)
 
 
-plt.legend(fontsize=12,frameon=False,loc='lower left')
+ax1.loglog(df_MuTau.iloc[a].ERs*1e6, df_MuTau.iloc[a].spec, label = 'g_x = ' + str(df_MuTau.iloc[a].g_x), color ='C0' )
+ax1.loglog(df_MuTau.iloc[b].ERs*1e6, df_MuTau.iloc[b].spec, label = 'g_x = ' + str(df_MuTau.iloc[b].g_x), color ='C1' )
+ax1.loglog(df_MuTau.iloc[c].ERs*1e6, df_MuTau.iloc[c].spec, label = 'g_x = ' + str(df_MuTau.iloc[c].g_x), color ='C2' )
+ax1.loglog(df_MuTau.iloc[d].ERs*1e6, df_MuTau.iloc[d].spec, label = 'g_x = ' + str(df_MuTau.iloc[d].g_x), color ='C3' )
+ax1.loglog(df_MuTau.iloc[e].ERs*1e6, df_MuTau.iloc[e].spec, label = 'g_x = ' + str(df_MuTau.iloc[e].g_x), color ='C4' )
+ax1.loglog(df_MuTau.iloc[g].ERs*1e6, df_MuTau.iloc[g].spec, label = 'g_x = ' + str(df_MuTau.iloc[g].g_x), color ='C5' )
+
+ax1.legend(fontsize=12,frameon=False,loc='lower left')
+
+# Color graph of all points
+cmap = plt.get_cmap('Spectral')
+ax2.scatter(df_MuTau['m_A'].values, df_MuTau['g_x'].values, c=df_MuTau['diff'].values, norm=matplotlib.colors.LogNorm(), vmax = 2, cmap=cmap)
+ax2.set_ylabel('$g_x$ [GeV]', size=16)
+ax2.set_xlabel('$m_A$ [GeV]', size=16)
+ax2.set_xscale('log')
+ax2.set_yscale('log')
+sm =  ScalarMappable(norm=matplotlib.colors.LogNorm(vmin = 1e-2, vmax = 2), cmap=cmap)
+cbar = fig.colorbar(sm, ax=ax2)
+cbar.set_label('BSM/SM', rotation=270)
+
+# plotting points on colour graph
+ax2.plot(fixed_mass, df_MuTau.iloc[a].g_x, marker='o', markersize=10, color ='C0' )
+ax2.plot(fixed_mass, df_MuTau.iloc[b].g_x, marker='o', markersize=10, color ='C1' )
+ax2.plot(fixed_mass, df_MuTau.iloc[c].g_x, marker='o', markersize=10, color ='C2' )
+ax2.plot(fixed_mass, df_MuTau.iloc[d].g_x, marker='o', markersize=10, color ='C3' )
+ax2.plot(fixed_mass, df_MuTau.iloc[e].g_x, marker='o', markersize=10, color ='C4' )
+ax2.plot(fixed_mass, df_MuTau.iloc[g].g_x, marker='o', markersize=10, color ='C5' )
+
+plt.show()
+
 
 #%% M2 PLOTTING BSM/SM vs ER
 
-def getratio(index):
-    '''
-    returns ratio of bsm/sm for a given row ion df
-    
-    index: index of row in df
 
-    '''
-    # points at which we want to sample
-    x_sample = sm_er # sm spec starts at a higher ER
-    ratio = np.interp(x_sample, df_MuTau.iloc[index].ERs, df_MuTau.iloc[index].spec) / sm_spec
-    
-    return ratio
     
 f=plt.figure(50,figsize=(10,8))
 
@@ -173,18 +198,19 @@ plt.axhline(y=1, color='r', linestyle='--')
 
 #plt.ylim(0, 10)
 
-plt.plot(sm_er*1e6, getratio(a) , label = 'g_x = ' + str(df_MuTau.iloc[a].g_x))
-plt.plot(sm_er*1e6, getratio(b) , label = 'g_x = ' + str(df_MuTau.iloc[b].g_x))
-plt.plot(sm_er*1e6, getratio(c) , label = 'g_x = ' + str(df_MuTau.iloc[c].g_x))
-plt.plot(sm_er*1e6, getratio(d) , label = 'g_x = ' + str(df_MuTau.iloc[d].g_x))
-plt.plot(sm_er*1e6, getratio(e) , label = 'g_x = ' + str(df_MuTau.iloc[e].g_x))
-plt.plot(sm_er*1e6, getratio(g) , label = 'g_x = ' + str(df_MuTau.iloc[g].g_x))
+plt.plot(sm_er*1e6, getratio(a) , label = 'g_x = ' + str(df_MuTau.iloc[a].g_x), color ='C0')
+plt.plot(sm_er*1e6, getratio(b) , label = 'g_x = ' + str(df_MuTau.iloc[b].g_x), color ='C1')
+plt.plot(sm_er*1e6, getratio(c) , label = 'g_x = ' + str(df_MuTau.iloc[c].g_x), color ='C2')
+plt.plot(sm_er*1e6, getratio(d) , label = 'g_x = ' + str(df_MuTau.iloc[d].g_x), color ='C3')
+plt.plot(sm_er*1e6, getratio(e) , label = 'g_x = ' + str(df_MuTau.iloc[e].g_x), color ='C4')
+plt.plot(sm_er*1e6, getratio(g) , label = 'g_x = ' + str(df_MuTau.iloc[g].g_x), color ='C5')
 
-plt.title(' m_A = ' + str(fixed_mass))
+plt.title('Fixed mass: m_A = ' + str(fixed_mass) + ' [GeV]')
 plt.xlabel(r'$E_R\,\,\,\left[\rm{keV}\right]$', size=16)
 plt.ylabel(r'BSM/SM', size=16)
 
 plt.xscale('log')
+plt.yscale('log')
 
 plt.legend(fontsize=12,frameon=False,loc='upper right')
 
@@ -201,21 +227,43 @@ g = getindex(fixed_g, 1)
 
 
 # plotting
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(25,10))
+fig.suptitle('Fixed coupling: g_x =' + str(fixed_g) + ' [GeV]', fontsize = 20)
 
-f=plt.figure(6,figsize=(10,8))
-plt.xlabel(r'$E_R\,\,\,\left[\rm{keV}\right]$', size=16)
-plt.ylabel(r'$\rm{d}R/\rm{d}E_R\,\,\,\left[\rm{keV}^{-1}\,\,\rm{ton}^{-1}\,\,\rm{yr}^{-1}\right]$', size=16)
+# Er spec for the 6 points
+ax1.set_xlabel(r'$E_R\,\,\,\left[\rm{keV}\right]$', size=16)
+ax1.set_ylabel(r'$\rm{d}R/\rm{d}E_R\,\,\,\left[\rm{keV}^{-1}\,\,\rm{ton}^{-1}\,\,\rm{yr}^{-1}\right]$', size=16)
 
-plt.title(' g_x = ' + str(fixed_g))
 
-plt.loglog(df_MuTau.iloc[a].ERs*1e6, df_MuTau.iloc[a].spec, label = ' m_A = ' + str(df_MuTau.iloc[a].m_A))
-plt.loglog(df_MuTau.iloc[b].ERs*1e6, df_MuTau.iloc[b].spec, label = ' m_A = ' + str(df_MuTau.iloc[b].m_A))
-plt.loglog(df_MuTau.iloc[c].ERs*1e6, df_MuTau.iloc[c].spec, label = ' m_A = ' + str(df_MuTau.iloc[c].m_A))
-plt.loglog(df_MuTau.iloc[d].ERs*1e6, df_MuTau.iloc[d].spec, label = ' m_A = ' + str(df_MuTau.iloc[d].m_A))
-plt.loglog(df_MuTau.iloc[e].ERs*1e6, df_MuTau.iloc[e].spec, label = ' m_A = ' + str(df_MuTau.iloc[e].m_A))
-plt.loglog(df_MuTau.iloc[g].ERs*1e6, df_MuTau.iloc[g].spec, label = ' m_A = ' + str(df_MuTau.iloc[g].m_A))
+ax1.loglog(df_MuTau.iloc[a].ERs*1e6, df_MuTau.iloc[a].spec, label = ' m_A = ' + str(df_MuTau.iloc[a].m_A), color ='C0' )
+ax1.loglog(df_MuTau.iloc[b].ERs*1e6, df_MuTau.iloc[b].spec, label = ' m_A = ' + str(df_MuTau.iloc[b].m_A), color ='C1' )
+ax1.loglog(df_MuTau.iloc[c].ERs*1e6, df_MuTau.iloc[c].spec, label = ' m_A = ' + str(df_MuTau.iloc[c].m_A), color ='C2' )
+ax1.loglog(df_MuTau.iloc[d].ERs*1e6, df_MuTau.iloc[d].spec, label = ' m_A = ' + str(df_MuTau.iloc[d].m_A), color ='C3' )
+ax1.loglog(df_MuTau.iloc[e].ERs*1e6, df_MuTau.iloc[e].spec, label = ' m_A = ' + str(df_MuTau.iloc[e].m_A), color ='C4' )
+ax1.loglog(df_MuTau.iloc[g].ERs*1e6, df_MuTau.iloc[g].spec, label = ' m_A = ' + str(df_MuTau.iloc[g].m_A), color ='C5' )
 
-plt.legend(fontsize=12,frameon=False,loc='lower left')
+ax1.legend(fontsize=12,frameon=False,loc='lower left')
+
+# Color graph of all points
+cmap = plt.get_cmap('Spectral')
+ax2.scatter(df_MuTau['m_A'].values, df_MuTau['g_x'].values, c=df_MuTau['diff'].values, norm=matplotlib.colors.LogNorm(), vmax = 2, cmap=cmap)
+ax2.set_ylabel('$g_x$ [GeV]', size=16)
+ax2.set_xlabel('$m_A$ [GeV]', size=16)
+ax2.set_xscale('log')
+ax2.set_yscale('log')
+sm =  ScalarMappable(norm=matplotlib.colors.LogNorm(vmin = 1e-2, vmax = 2), cmap=cmap)
+cbar = fig.colorbar(sm, ax=ax2)
+cbar.set_label('BSM/SM', rotation=270)
+
+# plotting points on colour graph
+ax2.plot(df_MuTau.iloc[a].m_A, df_MuTau.iloc[a].g_x, marker='o', markersize=10, color ='C0' )
+ax2.plot(df_MuTau.iloc[b].m_A, df_MuTau.iloc[b].g_x, marker='o', markersize=10, color ='C1' )
+ax2.plot(df_MuTau.iloc[c].m_A, df_MuTau.iloc[c].g_x, marker='o', markersize=10, color ='C2' )
+ax2.plot(df_MuTau.iloc[d].m_A, df_MuTau.iloc[d].g_x, marker='o', markersize=10, color ='C3' )
+ax2.plot(df_MuTau.iloc[e].m_A, df_MuTau.iloc[e].g_x, marker='o', markersize=10, color ='C4' )
+ax2.plot(df_MuTau.iloc[g].m_A, df_MuTau.iloc[g].g_x, marker='o', markersize=10, color ='C5' )
+
+plt.show()
 
 
 #%% G2 Plotting ratio for fixed coupling
@@ -223,20 +271,21 @@ f=plt.figure(51,figsize=(10,8))
 # line at 1
 plt.axhline(y=1, color='r', linestyle='--')
 
-plt.ylim(0, 10)
 
-plt.plot(sm_er*1e6, getratio(a) , label = ' m_A = ' + str(df_MuTau.iloc[a].m_A))
-plt.plot(sm_er*1e6, getratio(b) , label = ' m_A = ' + str(df_MuTau.iloc[b].m_A))
-plt.plot(sm_er*1e6, getratio(c) , label = ' m_A = ' + str(df_MuTau.iloc[c].m_A))
-plt.plot(sm_er*1e6, getratio(d) , label = ' m_A = ' + str(df_MuTau.iloc[d].m_A))
-plt.plot(sm_er*1e6, getratio(e) , label = ' m_A = ' + str(df_MuTau.iloc[e].m_A))
-plt.plot(sm_er*1e6, getratio(g) , label = ' m_A = ' + str(df_MuTau.iloc[g].m_A))
 
-plt.title(' g_x = ' + str(fixed_g))
+plt.plot(sm_er*1e6, getratio(a) , label = ' m_A = ' + str(df_MuTau.iloc[a].m_A), color ='C0')
+plt.plot(sm_er*1e6, getratio(b) , label = ' m_A = ' + str(df_MuTau.iloc[b].m_A), color ='C1')
+plt.plot(sm_er*1e6, getratio(c) , label = ' m_A = ' + str(df_MuTau.iloc[c].m_A), color ='C2')
+plt.plot(sm_er*1e6, getratio(d) , label = ' m_A = ' + str(df_MuTau.iloc[d].m_A), color ='C3')
+plt.plot(sm_er*1e6, getratio(e) , label = ' m_A = ' + str(df_MuTau.iloc[e].m_A), color ='C4')
+plt.plot(sm_er*1e6, getratio(g) , label = ' m_A = ' + str(df_MuTau.iloc[g].m_A), color ='C5')
+
+plt.title('Fixed coupling: g_x = ' + str(fixed_g) + ' [GeV]')
 plt.xlabel(r'$E_R\,\,\,\left[\rm{keV}\right]$', size=16)
 plt.ylabel(r'BSM/SM', size=16)
 
 plt.xscale('log')
+plt.yscale('log')
 
 plt.legend(fontsize=12,frameon=False,loc='upper right')
 
@@ -244,7 +293,7 @@ plt.legend(fontsize=12,frameon=False,loc='upper right')
 
 
 
-# In[ ]: SELECTING VALUES
+# In[ ]: SELECTING VALUES  - BENCHMARK POINTS
      
 # selecting values - starred points
 m = 1e-2
@@ -260,23 +309,73 @@ g = getindex(4e-4, m)
 print(a,b,c,d,e,g)
 
 #%%
+
 # plotting
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(25,10))
+fig.suptitle('Benchmark Points', size=20)
 
-f=plt.figure(7,figsize=(10,8))
+# Er spec for the 6 points
+ax1.set_xlabel(r'$E_R\,\,\,\left[\rm{keV}\right]$', size=16)
+ax1.set_ylabel(r'$\rm{d}R/\rm{d}E_R\,\,\,\left[\rm{keV}^{-1}\,\,\rm{ton}^{-1}\,\,\rm{yr}^{-1}\right]$', size=16)
+
+
+ax1.loglog(df_MuTau.iloc[a].ERs*1e6, df_MuTau.iloc[a].spec, label = 'g_x = ' + str(df_MuTau.iloc[a].g_x) + ', m_A = ' + str(df_MuTau.iloc[a].m_A), color ='C0' )
+ax1.loglog(df_MuTau.iloc[b].ERs*1e6, df_MuTau.iloc[b].spec, label = 'g_x = ' + str(df_MuTau.iloc[b].g_x) + ', m_A = ' + str(df_MuTau.iloc[b].m_A), color ='C1' )
+ax1.loglog(df_MuTau.iloc[c].ERs*1e6, df_MuTau.iloc[c].spec, label = 'g_x = ' + str(df_MuTau.iloc[c].g_x) + ', m_A = ' + str(df_MuTau.iloc[c].m_A), color ='C2' )
+ax1.loglog(df_MuTau.iloc[d].ERs*1e6, df_MuTau.iloc[d].spec, label = 'g_x = ' + str(df_MuTau.iloc[d].g_x) + ', m_A = ' + str(df_MuTau.iloc[d].m_A), color ='C3' )
+ax1.loglog(df_MuTau.iloc[e].ERs*1e6, df_MuTau.iloc[e].spec, label = 'g_x = ' + str(df_MuTau.iloc[e].g_x) + ', m_A = ' + str(df_MuTau.iloc[e].m_A), color ='C4' )
+ax1.loglog(df_MuTau.iloc[g].ERs*1e6, df_MuTau.iloc[g].spec, label = 'g_x = ' + str(df_MuTau.iloc[g].g_x) + ', m_A = ' + str(df_MuTau.iloc[g].m_A), color ='C5' )
+
+ax1.legend(fontsize=12,frameon=False,loc='lower left')
+
+# Color graph of all points
+cmap = plt.get_cmap('Spectral')
+ax2.scatter(df_MuTau['m_A'].values, df_MuTau['g_x'].values, c=df_MuTau['diff'].values, norm=matplotlib.colors.LogNorm(), vmax = 2, cmap=cmap)
+ax2.set_ylabel('$g_x$ [GeV]', size=16)
+ax2.set_xlabel('$m_A$ [GeV]', size=16)
+ax2.set_xscale('log')
+ax2.set_yscale('log')
+sm =  ScalarMappable(norm=matplotlib.colors.LogNorm(vmin = 1e-2, vmax = 2), cmap=cmap)
+cbar = fig.colorbar(sm, ax=ax2)
+cbar.set_label('BSM/SM', rotation=270)
+
+# plotting points on colour graph
+ax2.plot(df_MuTau.iloc[a].m_A, df_MuTau.iloc[a].g_x, marker='o', markersize=10, color ='C0' )
+ax2.plot(df_MuTau.iloc[b].m_A, df_MuTau.iloc[b].g_x, marker='o', markersize=10, color ='C1' )
+ax2.plot(df_MuTau.iloc[c].m_A, df_MuTau.iloc[c].g_x, marker='o', markersize=10, color ='C2' )
+ax2.plot(df_MuTau.iloc[d].m_A, df_MuTau.iloc[d].g_x, marker='o', markersize=10, color ='C3' )
+ax2.plot(df_MuTau.iloc[e].m_A, df_MuTau.iloc[e].g_x, marker='o', markersize=10, color ='C4' )
+ax2.plot(df_MuTau.iloc[g].m_A, df_MuTau.iloc[g].g_x, marker='o', markersize=10, color ='C5' )
+
+plt.show()
+
+
+#%% RATIO OF BENCHMARK POINTS
+
+f=plt.figure(51,figsize=(10,8))
+# line at 1
+plt.axhline(y=1, color='r', linestyle='--')
+
+
+
+plt.plot(sm_er*1e6, getratio(a) , label = 'g_x = ' + str(df_MuTau.iloc[a].g_x) + ', m_A = ' + str(df_MuTau.iloc[a].m_A), color ='C0' )
+plt.plot(sm_er*1e6, getratio(b) , label = 'g_x = ' + str(df_MuTau.iloc[b].g_x) + ', m_A = ' + str(df_MuTau.iloc[b].m_A), color ='C1' )
+plt.plot(sm_er*1e6, getratio(c) , label = 'g_x = ' + str(df_MuTau.iloc[c].g_x) + ', m_A = ' + str(df_MuTau.iloc[c].m_A), color ='C2' )
+plt.plot(sm_er*1e6, getratio(d) , label = 'g_x = ' + str(df_MuTau.iloc[d].g_x) + ', m_A = ' + str(df_MuTau.iloc[d].m_A), color ='C3' )
+plt.plot(sm_er*1e6, getratio(e) , label = 'g_x = ' + str(df_MuTau.iloc[e].g_x) + ', m_A = ' + str(df_MuTau.iloc[e].m_A), color ='C4' )
+plt.plot(sm_er*1e6, getratio(g) , label = 'g_x = ' + str(df_MuTau.iloc[g].g_x) + ', m_A = ' + str(df_MuTau.iloc[g].m_A), color ='C5' )
+
+plt.title('Ratio: Benchmark points', size=20)
 plt.xlabel(r'$E_R\,\,\,\left[\rm{keV}\right]$', size=16)
-plt.ylabel(r'$\rm{d}R/\rm{d}E_R\,\,\,\left[\rm{keV}^{-1}\,\,\rm{ton}^{-1}\,\,\rm{yr}^{-1}\right]$', size=16)
+plt.ylabel(r'BSM/SM', size=16)
 
-plt.title("Benchmark Points")
+plt.xscale('log')
+plt.yscale('log')
 
-plt.loglog(df_MuTau.iloc[a].ERs*1e6, df_MuTau.iloc[a].spec, label = 'g_x = ' + str(df_MuTau.iloc[a].g_x) + ', m_A = ' + str(df_MuTau.iloc[a].m_A))
-plt.loglog(df_MuTau.iloc[b].ERs*1e6, df_MuTau.iloc[b].spec, label = 'g_x = ' + str(df_MuTau.iloc[b].g_x) + ', m_A = ' + str(df_MuTau.iloc[b].m_A))
-plt.loglog(df_MuTau.iloc[c].ERs*1e6, df_MuTau.iloc[c].spec, label = 'g_x = ' + str(df_MuTau.iloc[c].g_x) + ', m_A = ' + str(df_MuTau.iloc[c].m_A))
-plt.loglog(df_MuTau.iloc[d].ERs*1e6, df_MuTau.iloc[d].spec, label = 'g_x = ' + str(df_MuTau.iloc[d].g_x) + ', m_A = ' + str(df_MuTau.iloc[d].m_A))
-plt.loglog(df_MuTau.iloc[e].ERs*1e6, df_MuTau.iloc[e].spec, label = 'g_x = ' + str(df_MuTau.iloc[e].g_x) + ', m_A = ' + str(df_MuTau.iloc[e].m_A))
-plt.loglog(df_MuTau.iloc[g].ERs*1e6, df_MuTau.iloc[g].spec, label = 'g_x = ' + str(df_MuTau.iloc[g].g_x) + ', m_A = ' + str(df_MuTau.iloc[g].m_A))
+plt.legend(fontsize=12,frameon=False,loc='center left')
 
 
-plt.legend(fontsize=12,frameon=False,loc='lower left')
+
 
 
 # In[ ]: CONVERTING TO PE a
@@ -322,16 +421,16 @@ ax.set_yscale('log')
 ax.set_xlabel('Number of electrons',fontsize=26)
 ax.set_ylabel('Events per tyr',fontsize=26) 
 
-ax.plot(abins, acev, '-+',markersize=15, label = 'g_x = ' + str(df_MuTau.iloc[a].g_x) + ', m_A = ' + str(df_MuTau.iloc[a].m_A))
-ax.plot(bbins, bcev, '-+',markersize=15, label = 'g_x = ' + str(df_MuTau.iloc[b].g_x) + ', m_A = ' + str(df_MuTau.iloc[b].m_A))
-ax.plot(cbins, ccev, '-+',markersize=15, label = 'g_x = ' + str(df_MuTau.iloc[c].g_x) + ', m_A = ' + str(df_MuTau.iloc[c].m_A))
-ax.plot(dbins, dcev, '-+',markersize=15, label = 'g_x = ' + str(df_MuTau.iloc[d].g_x) + ', m_A = ' + str(df_MuTau.iloc[d].m_A))
-ax.plot(ebins, ecev, '-+',markersize=15, label = 'g_x = ' + str(df_MuTau.iloc[e].g_x) + ', m_A = ' + str(df_MuTau.iloc[e].m_A))
-ax.plot(gbins, gcev, '-+',markersize=15, label = 'g_x = ' + str(df_MuTau.iloc[g].g_x) + ', m_A = ' + str(df_MuTau.iloc[g].m_A))
+ax.plot(abins, acev, '-o',markersize=5, label = 'g_x = ' + str(df_MuTau.iloc[a].g_x) + ', m_A = ' + str(df_MuTau.iloc[a].m_A))
+ax.plot(bbins, bcev, '-o',markersize=5, label = 'g_x = ' + str(df_MuTau.iloc[b].g_x) + ', m_A = ' + str(df_MuTau.iloc[b].m_A))
+ax.plot(cbins, ccev, '-o',markersize=5, label = 'g_x = ' + str(df_MuTau.iloc[c].g_x) + ', m_A = ' + str(df_MuTau.iloc[c].m_A))
+ax.plot(dbins, dcev, '-o',markersize=5, label = 'g_x = ' + str(df_MuTau.iloc[d].g_x) + ', m_A = ' + str(df_MuTau.iloc[d].m_A))
+ax.plot(ebins, ecev, '-o',markersize=5, label = 'g_x = ' + str(df_MuTau.iloc[e].g_x) + ', m_A = ' + str(df_MuTau.iloc[e].m_A))
+ax.plot(gbins, gcev, '-o',markersize=5, label = 'g_x = ' + str(df_MuTau.iloc[g].g_x) + ', m_A = ' + str(df_MuTau.iloc[g].m_A))
 
 ax.set_xlim(0,65)
 
-ax.set_title('Benchmark Points')
+ax.set_title('Benchmark Points', size=20)
 
 ax.legend(fontsize=18,frameon=False,loc='upper right')
 
@@ -361,7 +460,7 @@ ax.plot(gbins, gcev/mycenns, '-',markersize=15, label = 'g_x = ' + str(df_MuTau.
 
 ax.set_xlim(0,50)
 
-ax.set_title('Benchmark Points Ratio')
+ax.set_title('Ratio: Benchmark Points', size=20)
 
 ax.legend(fontsize=18,frameon=False,loc='upper right')
 
